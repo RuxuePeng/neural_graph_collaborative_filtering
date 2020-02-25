b146cba8db514fd3a8d27f15da35ee9cd3c34deb
rpeng <ruxue.peng@sony.com>
2020-02-25 12:59:07 -0800

    bug fix and more comment

--- NGCF/utility/load_data.py
+++ NGCF/utility/load_data.py
@@ -1,24 +1,25 @@
-'''
+"""
 Created on Oct 10, 2018
 Tensorflow Implementation of Neural Graph Collaborative Filtering (NGCF) model in:
 Wang Xiang et al. Neural Graph Collaborative Filtering. In SIGIR 2019.
 
 @author: Xiang Wang (xiangwang@u.nus.edu)
-'''
+"""
 import numpy as np
 import random as rd
 import scipy.sparse as sp
 from time import time
+
 
 class Data(object):
     def __init__(self, path, batch_size):
         self.path = path
         self.batch_size = batch_size
 
-        train_file = path + '/train.txt'
-        test_file = path + '/test.txt'
+        train_file = path + "/train.txt"
+        test_file = path + "/test.txt"
 
-        #get number of users and items
+        # get number of users and items
         self.n_users, self.n_items = 0, 0
         self.n_train, self.n_test = 0, 0
         self.neg_pools = {}
@@ -28,7 +29,7 @@ class Data(object):
         with open(train_file) as f:
             for l in f.readlines():
                 if len(l) > 0:
-                    l = l.strip('\n').split(' ')
+                    l = l.strip("\n").split(" ")
                     items = [int(i) for i in l[1:]]
                     uid = int(l[0])
                     self.exist_users.append(uid)
@@ -39,9 +40,9 @@ class Data(object):
         with open(test_file) as f:
             for l in f.readlines():
                 if len(l) > 0:
-                    l = l.strip('\n')
+                    l = l.strip("\n")
                     try:
-                        items = [int(i) for i in l.split(' ')[1:]]
+                        items = [int(i) for i in l.split(" ")[1:]]
                     except Exception:
                         continue
                     self.n_items = max(self.n_items, max(items))
@@ -57,22 +58,24 @@ class Data(object):
         with open(train_file) as f_train:
             with open(test_file) as f_test:
                 for l in f_train.readlines():
-                    if len(l) == 0: break
-                    l = l.strip('\n')
-                    items = [int(i) for i in l.split(' ')]
+                    if len(l) == 0:
+                        break
+                    l = l.strip("\n")
+                    items = [int(i) for i in l.split(" ")]
                     uid, train_items = items[0], items[1:]
 
                     for i in train_items:
-                        self.R[uid, i] = 1.
+                        self.R[uid, i] = 1.0
                         # self.R[uid][i] = 1
 
                     self.train_items[uid] = train_items
 
                 for l in f_test.readlines():
-                    if len(l) == 0: break
-                    l = l.strip('\n')
+                    if len(l) == 0:
+                        break
+                    l = l.strip("\n")
                     try:
-                        items = [int(i) for i in l.split(' ')]
+                        items = [int(i) for i in l.split(" ")]
                     except Exception:
                         continue
 
@@ -82,28 +85,33 @@ class Data(object):
     def get_adj_mat(self):
         try:
             t1 = time()
-            adj_mat = sp.load_npz(self.path + '/s_adj_mat.npz')
-            norm_adj_mat = sp.load_npz(self.path + '/s_norm_adj_mat.npz')
-            mean_adj_mat = sp.load_npz(self.path + '/s_mean_adj_mat.npz')
-            print('already load adj matrix', adj_mat.shape, time() - t1)
+            adj_mat = sp.load_npz(self.path + "/s_adj_mat.npz")
+            norm_adj_mat = sp.load_npz(self.path + "/s_norm_adj_mat.npz")
+            mean_adj_mat = sp.load_npz(self.path + "/s_mean_adj_mat.npz")
+            print("already load adj matrix", adj_mat.shape, time() - t1)
 
         except Exception:
             adj_mat, norm_adj_mat, mean_adj_mat = self.create_adj_mat()
-            sp.save_npz(self.path + '/s_adj_mat.npz', adj_mat)
-            sp.save_npz(self.path + '/s_norm_adj_mat.npz', norm_adj_mat)
-            sp.save_npz(self.path + '/s_mean_adj_mat.npz', mean_adj_mat)
+            sp.save_npz(self.path + "/s_adj_mat.npz", adj_mat)
+            sp.save_npz(self.path + "/s_norm_adj_mat.npz", norm_adj_mat)
+            sp.save_npz(self.path + "/s_mean_adj_mat.npz", mean_adj_mat)
         return adj_mat, norm_adj_mat, mean_adj_mat
 
     def create_adj_mat(self):
         t1 = time()
-        adj_mat = sp.dok_matrix((self.n_users + self.n_items, self.n_users + self.n_items), dtype=np.float32)
+        # make a sparse matrix (Dictionary Of Keys)
+        adj_mat = sp.dok_matrix(
+            (self.n_users + self.n_items, self.n_users + self.n_items), dtype=np.float32
+        )
+        # convert sparse matrix to list of lists format
         adj_mat = adj_mat.tolil()
         R = self.R.tolil()
 
-        adj_mat[:self.n_users, self.n_users:] = R
-        adj_mat[self.n_users:, :self.n_users] = R.T
+        adj_mat[: self.n_users, self.n_users :] = R
+        adj_mat[self.n_users :, : self.n_users] = R.T
+        # convert back to sparse matrix (Dictionary Of Keys)
         adj_mat = adj_mat.todok()
-        print('already create adjacency matrix', adj_mat.shape, time() - t1)
+        print("already create adjacency matrix", adj_mat.shape, time() - t1)
 
         t2 = time()
 
@@ -111,12 +119,12 @@ class Data(object):
             rowsum = np.array(adj.sum(1))
 
             d_inv = np.power(rowsum, -1).flatten()
-            d_inv[np.isinf(d_inv)] = 0.
+            d_inv[np.isinf(d_inv)] = 0.0
             d_mat_inv = sp.diags(d_inv)
 
             norm_adj = d_mat_inv.dot(adj)
             # norm_adj = adj.dot(d_mat_inv)
-            print('generate single-normalized adjacency matrix.')
+            print("generate single-normalized adjacency matrix.")
             return norm_adj.tocoo()
 
         def check_adj_if_equal(adj):
@@ -124,13 +132,13 @@ class Data(object):
             degree = np.sum(dense_A, axis=1, keepdims=False)
 
             temp = np.dot(np.diag(np.power(degree, -1)), dense_A)
-            print('check normalized adjacency matrix whether equal to this laplacian matrix.')
+            print("check normalized adjacency matrix whether equal to this laplacian matrix.")
             return temp
 
         norm_adj_mat = normalized_adj_single(adj_mat + sp.eye(adj_mat.shape[0]))
         mean_adj_mat = normalized_adj_single(adj_mat)
 
-        print('already normalize adjacency matrix', time() - t2)
+        print("already normalize adjacency matrix", time() - t2)
         return adj_mat.tocsr(), norm_adj_mat.tocsr(), mean_adj_mat.tocsr()
 
     def negative_pool(self):
@@ -139,7 +147,7 @@ class Data(object):
             neg_items = list(set(range(self.n_items)) - set(self.train_items[u]))
             pools = [rd.choice(neg_items) for _ in range(100)]
             self.neg_pools[u] = pools
-        print('refresh negative pools', time() - t1)
+        print("refresh negative pools", time() - t1)
 
     def sample(self):
         if self.batch_size <= self.n_users:
@@ -147,13 +155,13 @@ class Data(object):
         else:
             users = [rd.choice(self.exist_users) for _ in range(self.batch_size)]
 
-
         def sample_pos_items_for_u(u, num):
             pos_items = self.train_items[u]
             n_pos_items = len(pos_items)
             pos_batch = []
             while True:
-                if len(pos_batch) == num: break
+                if len(pos_batch) == num:
+                    break
                 pos_id = np.random.randint(low=0, high=n_pos_items, size=1)[0]
                 pos_i_id = pos_items[pos_id]
 
@@ -164,8 +172,9 @@ class Data(object):
         def sample_neg_items_for_u(u, num):
             neg_items = []
             while True:
-                if len(neg_items) == num: break
-                neg_id = np.random.randint(low=0, high=self.n_items,size=1)[0]
+                if len(neg_items) == num:
+                    break
+                neg_id = np.random.randint(low=0, high=self.n_items, size=1)[0]
                 if neg_id not in self.train_items[u] and neg_id not in neg_items:
                     neg_items.append(neg_id)
             return neg_items
@@ -185,35 +194,39 @@ class Data(object):
         return self.n_users, self.n_items
 
     def print_statistics(self):
-        print('n_users=%d, n_items=%d' % (self.n_users, self.n_items))
-        print('n_interactions=%d' % (self.n_train + self.n_test))
-        print('n_train=%d, n_test=%d, sparsity=%.5f' % (self.n_train, self.n_test, (self.n_train + self.n_test)/(self.n_users * self.n_items)))
-
+        print("n_users=%d, n_items=%d" % (self.n_users, self.n_items))
+        print("n_interactions=%d" % (self.n_train + self.n_test))
+        print(
+            "n_train=%d, n_test=%d, sparsity=%.5f"
+            % (
+                self.n_train,
+                self.n_test,
+                (self.n_train + self.n_test) / (self.n_users * self.n_items),
+            )
+        )
 
     def get_sparsity_split(self):
         try:
             split_uids, split_state = [], []
-            lines = open(self.path + '/sparsity.split', 'r').readlines()
+            lines = open(self.path + "/sparsity.split", "r").readlines()
 
             for idx, line in enumerate(lines):
                 if idx % 2 == 0:
                     split_state.append(line.strip())
                     print(line.strip())
                 else:
-                    split_uids.append([int(uid) for uid in line.strip().split(' ')])
-            print('get sparsity split.')
+                    split_uids.append([int(uid) for uid in line.strip().split(" ")])
+            print("get sparsity split.")
 
         except Exception:
             split_uids, split_state = self.create_sparsity_split()
-            f = open(self.path + '/sparsity.split', 'w')
+            f = open(self.path + "/sparsity.split", "w")
             for idx in range(len(split_state)):
-                f.write(split_state[idx] + '\n')
-                f.write(' '.join([str(uid) for uid in split_uids[idx]]) + '\n')
-            print('create sparsity split.')
+                f.write(split_state[idx] + "\n")
+                f.write(" ".join([str(uid) for uid in split_uids[idx]]) + "\n")
+            print("create sparsity split.")
 
         return split_uids, split_state
-
-
 
     def create_sparsity_split(self):
         all_users_to_test = list(self.test_set.keys())
@@ -236,7 +249,7 @@ class Data(object):
         temp = []
         count = 1
         fold = 4
-        n_count = (self.n_train + self.n_test)
+        n_count = self.n_train + self.n_test
         n_rates = 0
 
         split_state = []
@@ -248,7 +261,11 @@ class Data(object):
             if n_rates >= count * 0.25 * (self.n_train + self.n_test):
                 split_uids.append(temp)
 
-                state = '#inter per user<=[%d], #users=[%d], #all rates=[%d]' %(n_iids, len(temp), n_rates)
+                state = "#inter per user<=[%d], #users=[%d], #all rates=[%d]" % (
+                    n_iids,
+                    len(temp),
+                    n_rates,
+                )
                 split_state.append(state)
                 print(state)
 
@@ -259,10 +276,12 @@ class Data(object):
             if idx == len(user_n_iid.keys()) - 1 or n_count == 0:
                 split_uids.append(temp)
 
-                state = '#inter per user<=[%d], #users=[%d], #all rates=[%d]' % (n_iids, len(temp), n_rates)
+                state = "#inter per user<=[%d], #users=[%d], #all rates=[%d]" % (
+                    n_iids,
+                    len(temp),
+                    n_rates,
+                )
                 split_state.append(state)
                 print(state)
-
-
 
         return split_uids, split_state
